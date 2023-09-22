239f206dfb760ebb2e0e5d1b6412041546ccc7bb
marszhhx <starzhang0755@gmail.com>
2023-09-22 09:43:59 -0700

    implemented search bar

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: '',
     };
+    console.log("constructor");
   }
 
   // get state(data) from API call
   componentDidMount() {
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users") // asynchronous
       .then((response) => response.json())
       .then((users) =>
@@ -30,9 +33,31 @@ class App extends Component {
 
   // render state in certain format
   render() {
+    console.log("render");
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchField = event.target.value.toLowerCase();
+            this.setState(
+              () => {
+                return { searchField };
+              },
+              () => {
+                console.log(this.state);
+              }
+            );
+          }}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
