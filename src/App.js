2911ac1c90862541ea940da160c72743e7cdbaa1
marszhhx <starzhang0755@gmail.com>
2023-09-29 20:37:49 -0700

    finish project

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ const App = () => {
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
+  console.log("rendered");
+
   useEffect(() => {
     console.log("effect fired");
     fetch("https://jsonplaceholder.typicode.com/users") // asynchronous
